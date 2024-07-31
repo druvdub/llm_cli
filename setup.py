35dc1be1db3f87f259f9f56dd901327db7e7f325
Dhruv <59387969+druvdub@users.noreply.github.com>
2024-08-01 01:10:38 +0530

    Update setup to use correct package directory + update get version method

--- setup.py
+++ setup.py
@@ -1,18 +1,29 @@
 from setuptools import setup, find_packages
-import importlib.metadata
+
+
+def get_version() -> str:
+    rel_path = "src/gemini_cli/__init__.py"
+    with open(rel_path, "r") as file:
+        for line in file.read().splitlines():
+            if line.startswith("__version__"):
+                delim = '"' if '"' in line else "'"
+                return line.split(delim)[1]
+    raise RuntimeError("Unable to find version string.")
+
 
 setup(
     name='gemini-cli',
     description='A CLI tool for interacting with the Gemini API',
-    version=importlib.metadata.version('gemini_cli'),
-    packages=find_packages(),
+    version=get_version(),
+    package_dir={"": "src"},
+    packages=find_packages("src"),
     install_requires=[
         'click>=8.1.0',
         'google-generativeai>=0.7.2'
     ],
     entry_points={
         'console_scripts': [
-            'gemini-cli=gemini_cli.cli:cli'
+            'gcli=gemini_cli.cli:cli'
         ]
     },
     author='ddos',
